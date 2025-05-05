cdd27b63cc6e9de49cc25131f6cccb91dd950c7f
maleek-s <malik1@windowslive.com>
2025-05-05 23:44:08 +0200

    fixed the digest setup

--- scrapers/helpers/sleep.js
+++
@@ -1,3 +0,0 @@
-export const sleep = (milliseconds) =>
-    new Promise((resolve) => setTimeout(resolve, milliseconds));
-  
--- scrapers/services/companyService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const fetchCompanies = async (url) => {
-  const response = await axios.get(url);
-  return response.data.careers;
-};
